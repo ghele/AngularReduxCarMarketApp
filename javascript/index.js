29b86662e20550525b894ba2e8e56d68bd3bd7af
ghele <gheletus_raoul@yahoo.com>
2016-11-18 17:49:47 +0200

    Added actions for add car and brand filter

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,35 @@
+
 const store = Redux.createStore( ( state = {}, action ) => {
     switch( action.type ) {
         case 'RECEIVE_CARS':
-            return Object.assign({}, state, action);
+            console.log(action.carData);
+            return Object.assign({}, state, {carData:action.carData}, {showData: action.carData.models});
+        case 'FILTER_BRAND':
+            console.log("STATE1", action.brand);
+
+            return Object.assign({}, state, {
+                showData: state.carData.models.filter( (value) => { return value.make === action.brand } )
+                                      });
+            // return Object.assign({}, state, { showData: {
+            //                               name: state.carData.name,
+            //                               models: state.carData.models.filter( (value) => { return value.make === action.brand} )
+            //                             }
+            //                           });
+        case 'FILTER_MODEL':
+            console.log("STATE", action);
+            if(action.model.length != 0) {
+              return Object.assign({}, state, {
+                  showData: state.carData.models.filter( (value) => { return value.name === action.model } )
+              });
+            } else {
+              return Object.assign({}, state, {
+                  showData: state.carData.models.filter( (value) => { return value.make === action.brand } )
+              });
+            }
+
         case 'ADD_CAR':
-            const isPicked = state.carData.models[action.carId].isSelected;
-            return Object.assign(state, isPicked = !isPicked);
-        case 'ADD_TO_CART':
-            return ;
+            Object.assign(state, state.carData.models[action.carId].isSelected = !state.carData.models[action.carId].isSelected);
+            return Object.assign({}, state, {cartData: state.carData.models.filter( (value) => { return value.isSelected === true } )})
         default:
             return state
     }
@@ -38,6 +61,9 @@ const calendarService = httpProvider => {
     }
 }
 
+/*
+ * Action CREATOR
+ */
 
 
 /*
@@ -48,28 +74,40 @@ const app = angular.module( 'test', [ ] )
 
 app.controller( 'CarMarketController', ( $scope, $timeout, calendar, store ) => {
     $scope.model = {
-        carData: [ ]
+        // carData: [ ]
     }
 
     store.subscribe( ( ) => {
         Object.assign( $scope.model, { list: store.getState( ) } )
-        // console.log("GET_STATE", store.getState());
+        console.log("GET_STATE", store.getState());
     } )
-
+    // $scope.$digest();
     $scope.getCars = ( ) => {
         calendar.getMarketCars( )
             .then( carData => store.dispatch( { type: 'RECEIVE_CARS', carData } ) )
     }
 
-    $scope.addCar = ( carId ) => {
-      console.log(carId);
-        store.dispatch( { type: 'ADD_CAR', carId } )
-        console.log("GET STATE", store.getState());
+    $scope.filterBrand = ( brand ) => {
+        store.dispatch( { type: 'FILTER_BRAND', brand } )
+    }
 
+    $scope.filterModel = ( model ) => {
+        store.dispatch( { type: 'FILTER_MODEL', model, brand } )
+    }
+
+    $scope.addCar = ( carId ) => {
+        store.dispatch( { type: 'ADD_CAR', carId } )
     }
 
     $scope.getCars();
+    $scope.addToCart = () => {
+      console.log(store.getState());
+    }
+    // addToCart();
 } )
+
+
+
 
 app.value( 'store', store )
 
